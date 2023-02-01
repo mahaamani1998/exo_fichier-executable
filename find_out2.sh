83370dc1b187cacb6700174ac62e39ca665b2d39
mahaamani1998 <124072869+mahaamani1998@users.noreply.github.com>
2023-02-01 12:46:31 +0100

    Add files via upload

---
+++ find_out2.sh
@@ -0,0 +1,19 @@
+exec=0
+non_exec=0
+read $doss
+for i in `ls $doss`
+
+do
+	if [ -x $i ]
+	then
+	let "exec+=1"
+	echo $i >>list_exec.txt
+
+	else
+	
+	let "non_exec+=1"
+	echo $i >>list_non_exec.txt
+	
+fi	
+done
+
